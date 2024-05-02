1c8dfe5d3150d8081340e9ae571a8a743e24df93
parsamre <p.mre@yahoo.com>
2024-05-02 10:56:09 -0400

    initial commit

---
+++ src/main/java/com/parsa/wedolist/WeDoListApplication.java
@@ -0,0 +1,13 @@
+package com.parsa.wedolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeDoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeDoListApplication.class, args);
+    }
+
+}
