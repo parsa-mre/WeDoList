7d3290ab187f5390cfca2e015fdcd211b4ef362d
parsamre <p.mre@yahoo.com>
2024-05-08 14:07:09 -0400

    add socket configuration

---
+++ src/main/java/com/parsa/wedolist/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package com.parsa.wedolist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    // Configuration for asynchronous processing
+}
